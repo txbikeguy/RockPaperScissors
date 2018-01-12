7e9f7ce6d6b3f8a71cc80ebdd0af9414dec9a443
Ben_Cross <txbikeguy@gmail.com>
2018-01-12 12:42:03 -0600

    Name Change

---
+++ Assets/Javascript/app.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyD2vtEDdW7ALhJSEDmH7b-LbkGRBSH6bUU",
+    authDomain: "first-project-4bfd6.firebaseapp.com",
+    databaseURL: "https://first-project-4bfd6.firebaseio.com",
+    projectId: "first-project-4bfd6",
+    storageBucket: "first-project-4bfd6.appspot.com",
+    messagingSenderId: "1061865414511"
+    };
+
+    firebase.initializeApp(config);
+
+    // Create a variable to reference the database
+    var database = firebase.database();
